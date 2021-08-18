e23976d9b3d881bbdbf5bef37de6d0b12be4586e
Eiji Mitsumizo <43096056+mitsumizo@users.noreply.github.com>
2021-08-18 09:20:58 +0900

    first commit

---
+++ src/main/java/bingo_goal/BingoGoalApplication.java
@@ -0,0 +1,13 @@
+package bingo_goal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoGoalApplication.class, args);
+	}
+
+}
