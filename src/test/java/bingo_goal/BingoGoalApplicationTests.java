330198f4ee7e3353275d68235c63243eaa4365b5
Eiji Mitsumizo <43096056+mitsumizo@users.noreply.github.com>
2021-08-18 09:20:58 +0900

    first commit

---
+++ src/test/java/bingo_goal/BingoGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package bingo_goal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoGoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
